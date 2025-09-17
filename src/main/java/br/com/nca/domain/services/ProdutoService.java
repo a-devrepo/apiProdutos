150a69f31dcbbfcefef608efb7e4c9f521473d04
alison <adevpr11@gmail.com>
2025-09-17 17:44:58 -0300

    feat: adiciona em ProdutoService método para listar

--- src/main/java/br/com/nca/domain/services/ProdutoService.java
+++ src/main/java/br/com/nca/domain/services/ProdutoService.java
@@ -1,5 +1,6 @@
 package br.com.nca.domain.services;
 
+import java.util.List;
 import java.util.UUID;
 
 import br.com.nca.domain.dtos.ObterProdutoDTO;
@@ -7,4 +8,6 @@ import br.com.nca.domain.dtos.ObterProdutoDTO;
 public interface ProdutoService {
 
 	ObterProdutoDTO buscarPorId(UUID id);
+
+	List<ObterProdutoDTO> listar();
 }
