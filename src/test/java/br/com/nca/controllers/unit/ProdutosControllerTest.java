c96f9296b2fcd9ff9f6d8e81e750d96e799763f1
alison <adevpr11@gmail.com>
2025-09-17 17:42:11 -0300

    test: cria teste para ProdutosController retornar lista de produtos

--- src/test/java/br/com/nca/controllers/unit/ProdutosControllerTest.java
+++ src/test/java/br/com/nca/controllers/unit/ProdutosControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 import java.util.UUID;
@@ -62,5 +63,46 @@ public class ProdutosControllerTest {
                     .andExpect(jsonPath("$.nome").value(produto.getNome()))
                     .andExpect(jsonPath("$.tipo").value(produto.getTipo().toString()))
                     .andExpect(jsonPath("$.precoUnitario").value(produto.getPrecoUnitario().toString()));
+	}
+	
+	@Test
+	@DisplayName("Deve retornar lista de produtos")
+	public void deveRetornarListaProdutos() throws Exception {
+		
+		var faker = new Faker(Locale.of("pt-BR"), new Random(93));
+		
+		UUID idProduto1 = UUID.randomUUID();
+		UUID idProduto2 = UUID.randomUUID();
+		
+		
+		var produtoDTO1 = ObterProdutoDTO.builder()
+				.id(idProduto1)
+				.nome(faker.commerce().productName())
+				.tipo(TipoProduto.MATERIAL)
+				.precoUnitario(new BigDecimal(faker.commerce().price(10.0, 500.0).replace(",", ".")))
+						.build();
+		
+		var produtoDTO2 = ObterProdutoDTO.builder()
+				.id(idProduto2)
+				.nome(faker.commerce().productName())
+				.tipo(TipoProduto.MATERIAL)
+				.precoUnitario(new BigDecimal(faker.commerce().price(10.0, 500.0).replace(",", ".")))
+						.build();
+		
+		var produtos = List.of(produtoDTO1, produtoDTO2);		
+		
+				when(produtoService.listar()).thenReturn(produtos);
+				
+				mockMvc.perform(get("/api/v1/produtos"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.length()").value(2))
+                    .andExpect(jsonPath("$[0].id").value(idProduto1.toString()))
+                    .andExpect(jsonPath("$[0].nome").value(produtoDTO1.getNome()))
+                    .andExpect(jsonPath("$[0].tipo").value(produtoDTO1.getTipo().toString()))
+                    .andExpect(jsonPath("$[0].precoUnitario").value(produtoDTO1.getPrecoUnitario().toString()))
+                    .andExpect(jsonPath("$[1].id").value(idProduto2.toString()))
+                    .andExpect(jsonPath("$[1].nome").value(produtoDTO2.getNome()))
+                    .andExpect(jsonPath("$[1].tipo").value(produtoDTO2.getTipo().toString()))
+                    .andExpect(jsonPath("$[1].precoUnitario").value(produtoDTO2.getPrecoUnitario().toString()));
 	}
 }
