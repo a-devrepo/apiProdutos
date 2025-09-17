75eaa586e1fa5e94bbbfa4abc42ea961c689e4a6
alison <adevpr11@gmail.com>
2025-09-17 18:58:02 -0300

    feat: implementa método cadastrar em ProdutosController

--- src/main/java/br/com/nca/controllers/ProdutosController.java
+++ src/main/java/br/com/nca/controllers/ProdutosController.java
@@ -3,12 +3,16 @@ package br.com.nca.controllers;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.nca.domain.dtos.CriarProdutoDTO;
 import br.com.nca.domain.dtos.ObterProdutoDTO;
 import br.com.nca.domain.services.ProdutoService;
 import lombok.RequiredArgsConstructor;
@@ -30,5 +34,11 @@ public class ProdutosController {
 	public ResponseEntity<List<ObterProdutoDTO>> listarProdutos() {
 		var produtos = produtoService.listar();
 		return ResponseEntity.ok(produtos);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ObterProdutoDTO> cadastrar(@RequestBody CriarProdutoDTO criarProdutoDTO) {
+		var obterProdutoDTO = produtoService.cadastrar(criarProdutoDTO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(obterProdutoDTO);
 	}
 }
