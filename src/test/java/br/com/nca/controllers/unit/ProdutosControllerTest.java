be2cbf4d53fec152e4b3e4ed19c0197f51c8ff0a
alison <adevpr11@gmail.com>
2025-09-17 14:20:45 -0300

    refactor: remove imports não utilizados

--- src/test/java/br/com/nca/controllers/unit/ProdutosControllerTest.java
+++ src/test/java/br/com/nca/controllers/unit/ProdutosControllerTest.java
@@ -2,7 +2,6 @@ package br.com.nca.controllers.unit;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -13,7 +12,6 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
