99bcba6ccdd281c51dd8ee6b094252ceddfb14ca
alison <adevpr11@gmail.com>
2025-09-17 12:46:02 -0300

    test: adiciona ProdutosControllerTest para testes unitários

---
+++ src/test/java/br/com/nca/controllers/unit/ProdutosControllerTest.java
@@ -0,0 +1,9 @@
+package br.com.nca.controllers.unit;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Testes unitários para ProdutosController")
+public class ProdutosControllerTest {
+
+	
+}
