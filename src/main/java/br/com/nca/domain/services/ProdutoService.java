78d55123553fb431f93f784f43b9c7877e18629e
alison <adevpr11@gmail.com>
2025-09-17 18:48:30 -0300

    feat: cria método cadastar na interface ProdutoService

--- src/main/java/br/com/nca/domain/services/ProdutoService.java
+++ src/main/java/br/com/nca/domain/services/ProdutoService.java
@@ -3,6 +3,7 @@ package br.com.nca.domain.services;
 import java.util.List;
 import java.util.UUID;
 
+import br.com.nca.domain.dtos.CriarProdutoDTO;
 import br.com.nca.domain.dtos.ObterProdutoDTO;
 
 public interface ProdutoService {
@@ -10,4 +11,6 @@ public interface ProdutoService {
 	ObterProdutoDTO buscarPorId(UUID id);
 
 	List<ObterProdutoDTO> listar();
+
+	ObterProdutoDTO cadastrar(CriarProdutoDTO criarProdutoDTO);
 }
