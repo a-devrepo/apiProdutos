5731d5c4ab5e4a9af8fbf74c8e9ea213c6efd830
alison <adevpr11@gmail.com>
2025-09-17 14:18:44 -0300

    test: ajuste de assert do precoUnitario

--- src/test/java/br/com/nca/controllers/unit/ProdutosControllerTest.java
+++ src/test/java/br/com/nca/controllers/unit/ProdutosControllerTest.java
@@ -1,5 +1,11 @@
 package br.com.nca.controllers.unit;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.math.BigDecimal;
 import java.util.Locale;
 import java.util.Random;
@@ -7,13 +13,22 @@ import java.util.UUID;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
 
+import br.com.nca.controllers.ProdutosController;
+import br.com.nca.domain.dtos.ObterProdutoDTO;
+import br.com.nca.domain.enums.TipoProduto;
+import br.com.nca.domain.services.ProdutoService;
+
+@AutoConfigureMockMvc
 @DisplayName("Testes unitários para ProdutosController")
 @WebMvcTest(ProdutosController.class)
 public class ProdutosControllerTest {
@@ -29,7 +44,7 @@ public class ProdutosControllerTest {
 	
 	@Test
 	@DisplayName("Deve retornar produto por ID")
-	public void deveRetornarProdutoPorId() {
+	public void deveRetornarProdutoPorId() throws Exception {
 		
 		var faker = new Faker(Locale.of("pt-BR"), new Random(93));
 		
@@ -38,18 +53,16 @@ public class ProdutosControllerTest {
 				.id(id)
 				.nome(faker.commerce().productName())
 				.tipo(TipoProduto.MATERIAL)
-				.precoUnitario(new BigDecimal(faker.commerce().price(10.0, 500.0))
+				.precoUnitario(new BigDecimal(faker.commerce().price(10.0, 500.0).replace(",", ".")))
 						.build();
 				
 				when(produtoService.buscarPorId(id)).thenReturn(produto);
 				
 				mockMvc.perform(get("/api/v1/produtos/{id}", id))
                     .andExpect(status().isOk())
-                    .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                     .andExpect(jsonPath("$.id").value(id.toString()))
                     .andExpect(jsonPath("$.nome").value(produto.getNome()))
                     .andExpect(jsonPath("$.tipo").value(produto.getTipo().toString()))
-                    .andExpect(jsonPath("$.precoUnitario").value(produto.getPrecoUnitario()));
+                    .andExpect(jsonPath("$.precoUnitario").value(produto.getPrecoUnitario().toString()));
 	}
-	
 }
