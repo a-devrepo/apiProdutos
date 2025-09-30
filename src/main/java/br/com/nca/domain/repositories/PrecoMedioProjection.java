7949919bb14a52050574b7cace515eff1cb06c08
alison <adevpr11@gmail.com>
2025-09-30 14:03:49 -0300

    feat: adiciona em ProdutoRepository método para obter projeção de preço médio por tipo

--- src/main/java/br/com/nca/domain/repositories/PrecoMedioProjection.java
+++ src/main/java/br/com/nca/domain/repositories/PrecoMedioProjection.java
@@ -1,4 +1,12 @@
 package br.com.nca.domain.repositories;
 
+import br.com.nca.domain.enums.TipoProduto;
+
+import java.math.BigDecimal;
+
 public interface PrecoMedioProjection {
+    String getNome();
+    TipoProduto getTipo();
+    Integer getQuantidade();
+    BigDecimal getPrecoMedio();
 }
