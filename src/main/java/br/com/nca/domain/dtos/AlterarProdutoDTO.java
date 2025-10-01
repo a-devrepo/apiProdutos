c20f3e9f109a654add9ea8f7d43fa0af684f40eb
alison <adevpr11@gmail.com>
2025-09-30 22:38:34 -0300

    feat: validação de DTO de alteração

--- src/main/java/br/com/nca/domain/dtos/AlterarProdutoDTO.java
+++ src/main/java/br/com/nca/domain/dtos/AlterarProdutoDTO.java
@@ -1,18 +1,29 @@
 package br.com.nca.domain.dtos;
 
-import java.math.BigDecimal;
-import java.util.UUID;
-
 import br.com.nca.domain.enums.TipoProduto;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
 @Builder
 public class AlterarProdutoDTO {
 
-	private UUID id;
-	private String nome;
-	private TipoProduto tipo;
-	private BigDecimal precoUnitario;
+    @NotNull(message = "O id é obrigatório")
+    private UUID id;
+
+    @Size(min = 6, max = 100, message = "Nome deve ter entre 5 e 100 caracteres.")
+    @Pattern(regexp = "^[A-Za-zÀ-ú0-9 ]+$", message = "Nome deve conter apenas letras, números e espaços.")
+    private String nome;
+
+    private TipoProduto tipo;
+
+    @DecimalMin(value = "0.01", message = "O preço deve ser maior que zero")
+    private BigDecimal precoUnitario;
 }
